e8972be272eb1841aef99301d22885d80bc5b6a9
almirjuniior <57836556+almirjuniior@users.noreply.github.com>
2022-04-13 16:43:11 -0300

    Delete projectajr directory

--- projectajr/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-/* MODO ESCURO */
-
-function mudartema() {
-
-    var body = document.getElementById("body");
-    var currentClass = body.className;
-    body.className = currentClass == "night" ? "day" : "night";
-  
-}
